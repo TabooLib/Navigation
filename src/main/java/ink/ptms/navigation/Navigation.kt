735f1c5869d10b652c105c58d0d1052b0397ca5a
sky <sky@asgard.me>
2021-03-01 15:11:49 +0800

    update

--- src/main/java/ink/ptms/navigation/Navigation.kt
+++ src/main/java/ink/ptms/navigation/Navigation.kt
@@ -5,7 +5,7 @@ import ink.ptms.navigation.pathfinder.NodeReader
 import ink.ptms.navigation.pathfinder.PathFinder
 import io.izzel.taboolib.loader.Plugin
 
-object Navigation : Plugin() {
+object Navigation {
 
     fun create(nodeEntity: NodeEntity): PathFinder {
         return PathFinder(NodeReader(nodeEntity))
