279c9cde36e17f086d106e4a418ff69d09f450bb
TONY_All <admin@minicraft.top>
2021-03-27 17:03:34 +0800

    add Lib for legacy

--- src/main/java/ink/ptms/navigation/Navigation.kt
+++ src/main/java/ink/ptms/navigation/Navigation.kt
@@ -3,8 +3,16 @@ package ink.ptms.navigation
 import ink.ptms.navigation.pathfinder.NodeEntity
 import ink.ptms.navigation.pathfinder.NodeReader
 import ink.ptms.navigation.pathfinder.PathFinder
+import io.izzel.taboolib.Version
+import io.izzel.taboolib.module.dependency.TDependency
 
 object Navigation {
+
+    init {
+        if (Version.isBefore(Version.v1_16)) {
+            TDependency.requestLib("it.unimi.dsi:fastutil:8.5.2", TDependency.MAVEN_REPO, "")
+        }
+    }
 
     fun create(nodeEntity: NodeEntity): PathFinder {
         return PathFinder(NodeReader(nodeEntity))
