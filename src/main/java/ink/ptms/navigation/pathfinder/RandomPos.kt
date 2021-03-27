9abd4ea44663bc42dc9a9df2d838a7ec18cd92a9
TONY_All <admin@minicraft.top>
2021-03-27 16:34:27 +0800

    fix name of params code cleanup

--- src/main/java/ink/ptms/navigation/pathfinder/RandomPos.kt
+++ src/main/java/ink/ptms/navigation/pathfinder/RandomPos.kt
@@ -8,7 +8,6 @@ import ink.ptms.navigation.util.up
 import io.izzel.taboolib.module.nms.impl.Position
 import org.bukkit.Material
 import org.bukkit.util.Vector
-import java.lang.IllegalArgumentException
 import java.util.*
 import java.util.function.Predicate
 import java.util.function.ToDoubleFunction
@@ -18,16 +17,16 @@ object RandomPos {
     private const val SQRT_OF_TWO = 1.4142135623730951
 
     @Nullable
-    fun getPos(aqr: NodeEntity, integer2: Int, integer3: Int): Vector? {
+    fun getPos(nodeEntity: NodeEntity, integer2: Int, integer3: Int): Vector? {
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
             null,
             true,
             1.5707963705062866,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             false,
             0,
             0,
@@ -37,22 +36,22 @@ object RandomPos {
 
     @Nullable
     fun getAirPos(
-        aqr: NodeEntity,
+        nodeEntity: NodeEntity,
         integer2: Int,
         integer3: Int,
         integer4: Int,
-        dck: Vector,
+        vector: Vector,
         double6: Double
     ): Vector? {
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             integer4,
-            dck,
+            vector,
             true,
             double6,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             true,
             0,
             0,
@@ -61,14 +60,19 @@ object RandomPos {
     }
 
     @Nullable
-    fun getLandPos(aqr: NodeEntity, integer2: Int, integer3: Int): Vector? {
-        return getLandPos(aqr, integer2, integer3, aqr::getWalkTargetValue)
+    fun getLandPos(nodeEntity: NodeEntity, integer2: Int, integer3: Int): Vector? {
+        return getLandPos(nodeEntity, integer2, integer3, nodeEntity::getWalkTargetValue)
     }
 
     @Nullable
-    fun getLandPos(aqr: NodeEntity, integer2: Int, integer3: Int, toDoubleFunction: ToDoubleFunction<Position>): Vector? {
+    fun getLandPos(
+        nodeEntity: NodeEntity,
+        integer2: Int,
+        integer3: Int,
+        toDoubleFunction: ToDoubleFunction<Position>
+    ): Vector? {
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
@@ -85,32 +89,32 @@ object RandomPos {
 
     @Nullable
     fun getAboveLandPos(
-        aqr: NodeEntity,
+        nodeEntity: NodeEntity,
         integer2: Int,
         integer3: Int,
-        dck: Vector,
+        vector: Vector,
         float5: Float,
         integer6: Int,
         integer7: Int
     ): Vector? {
         return generateRandomPos(
-            aqr, integer2, integer3, 0, dck, false,
-            float5.toDouble(), aqr::getWalkTargetValue, true, integer6, integer7, true
+            nodeEntity, integer2, integer3, 0, vector, false,
+            float5.toDouble(), nodeEntity::getWalkTargetValue, true, integer6, integer7, true
         )
     }
 
     @Nullable
-    fun getLandPosTowards(aqr: NodeEntity, integer2: Int, integer3: Int, dck: Vector): Vector? {
-        val dck5 = dck.subtract(Vector(aqr.x, aqr.y, aqr.z))
+    fun getLandPosTowards(nodeEntity: NodeEntity, integer2: Int, integer3: Int, vector: Vector): Vector? {
+        val dck5 = vector.subtract(Vector(nodeEntity.x, nodeEntity.y, nodeEntity.z))
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
             dck5,
             false,
             1.5707963705062866,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             true,
             0,
             0,
@@ -119,17 +123,17 @@ object RandomPos {
     }
 
     @Nullable
-    fun getPosTowards(aqr: NodeEntity, integer2: Int, integer3: Int, dck: Vector): Vector? {
-        val dck5 = dck.subtract(Vector(aqr.x, aqr.y, aqr.z))
+    fun getPosTowards(nodeEntity: NodeEntity, integer2: Int, integer3: Int, vector: Vector): Vector? {
+        val dck5 = vector.subtract(Vector(nodeEntity.x, nodeEntity.y, nodeEntity.z))
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
             dck5,
             true,
             1.5707963705062866,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             false,
             0,
             0,
@@ -138,17 +142,17 @@ object RandomPos {
     }
 
     @Nullable
-    fun getPosTowards(aqr: NodeEntity, integer2: Int, integer3: Int, dck: Vector, double5: Double): Vector? {
-        val dck7 = dck.subtract(Vector(aqr.x, aqr.y, aqr.z))
+    fun getPosTowards(nodeEntity: NodeEntity, integer2: Int, integer3: Int, vector: Vector, double5: Double): Vector? {
+        val dck7 = vector.subtract(Vector(nodeEntity.x, nodeEntity.y, nodeEntity.z))
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
             dck7,
             true,
             double5,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             false,
             0,
             0,
@@ -158,23 +162,23 @@ object RandomPos {
 
     @Nullable
     fun getAirPosTowards(
-        aqr: NodeEntity,
+        nodeEntity: NodeEntity,
         integer2: Int,
         integer3: Int,
         integer4: Int,
-        dck: Vector,
+        vector: Vector,
         double6: Double
     ): Vector? {
-        val dck8 = dck.subtract(Vector(aqr.x, aqr.y, aqr.z))
+        val dck8 = vector.subtract(Vector(nodeEntity.x, nodeEntity.y, nodeEntity.z))
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             integer4,
             dck8,
             false,
             double6,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             true,
             0,
             0,
@@ -183,17 +187,17 @@ object RandomPos {
     }
 
     @Nullable
-    fun getPosAvoid(aqr: NodeEntity, integer2: Int, integer3: Int, dck: Vector): Vector? {
-        val dck5 = aqr.location.subtract(dck).toVector()
+    fun getPosAvoid(nodeEntity: NodeEntity, integer2: Int, integer3: Int, vector: Vector): Vector? {
+        val dck5 = nodeEntity.location.subtract(vector).toVector()
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
             dck5,
             true,
             1.5707963705062866,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             false,
             0,
             0,
@@ -202,17 +206,17 @@ object RandomPos {
     }
 
     @Nullable
-    fun getLandPosAvoid(aqr: NodeEntity, integer2: Int, integer3: Int, dck: Vector): Vector? {
-        val dck5 = aqr.location.subtract(dck).toVector()
+    fun getLandPosAvoid(nodeEntity: NodeEntity, integer2: Int, integer3: Int, vector: Vector): Vector? {
+        val dck5 = nodeEntity.location.subtract(vector).toVector()
         return generateRandomPos(
-            aqr,
+            nodeEntity,
             integer2,
             integer3,
             0,
             dck5,
             false,
             1.5707963705062866,
-            aqr::getWalkTargetValue,
+            nodeEntity::getWalkTargetValue,
             true,
             0,
             0,
@@ -225,7 +229,7 @@ object RandomPos {
         integer2: Int,
         integer3: Int,
         integer4: Int,
-        dck: Vector?,
+        vector: Vector?,
         boolean6: Boolean,
         double7: Double,
         toDoubleFunction: ToDoubleFunction<Position>,
@@ -234,83 +238,96 @@ object RandomPos {
         integer11: Int,
         boolean12: Boolean
     ): Vector? {
-        val random15: Random = nodeEntity.random
+        val random: Random = nodeEntity.random
 
         val navigation = PathTypeFactory(nodeEntity)
 
-        val boolean16: Boolean = if (nodeEntity.hasRestriction) {
-            nodeEntity.restrictCenter.closerThan(nodeEntity.location.toPosition(), (nodeEntity.restrictRadius + integer2.toFloat()).toDouble() + 1.0)
+        val hasRestriction: Boolean = if (nodeEntity.hasRestriction) {
+            nodeEntity.restrictCenter.closerThan(
+                nodeEntity.location.toPosition(),
+                (nodeEntity.restrictRadius + integer2.toFloat()).toDouble() + 1.0
+            )
         } else {
             false
         }
 
-        var boolean17 = false
+        var bool = false
 
         var double18 = Double.NEGATIVE_INFINITY
 
-        var fx20: Position = nodeEntity.location.toPosition()
+        var position = nodeEntity.location.toPosition()
 
-        for (integer21 in 0..9) {
-            val fx22: Position? = getRandomDelta(random15, integer2, integer3, integer4, dck, double7)
-            if (fx22 != null) {
-                var integer23: Int = fx22.x
-                val integer24: Int = fx22.y
-                var integer25: Int = fx22.z
-                var fx26: Position
+        for (i in 0..9) {
+            val delta: Position? = getRandomDelta(random, integer2, integer3, integer4, vector, double7)
+            if (delta != null) {
+                var x = delta.x
+                val y = delta.y
+                var z = delta.z
+                var result: Position
                 if (nodeEntity.hasRestriction && integer2 > 1) {
-                    fx26 = nodeEntity.restrictCenter
-                    if (nodeEntity.x > fx26.x) {
-                        integer23 -= random15.nextInt(integer2 / 2)
+                    result = nodeEntity.restrictCenter
+                    if (nodeEntity.x > result.x) {
+                        x -= random.nextInt(integer2 / 2)
                     } else {
-                        integer23 += random15.nextInt(integer2 / 2)
+                        x += random.nextInt(integer2 / 2)
                     }
-                    if (nodeEntity.z > fx26.z) {
-                        integer25 -= random15.nextInt(integer2 / 2)
+                    if (nodeEntity.z > result.z) {
+                        z -= random.nextInt(integer2 / 2)
                     } else {
-                        integer25 += random15.nextInt(integer2 / 2)
+                        z += random.nextInt(integer2 / 2)
                     }
                 }
 
-                fx26 = Position(
-                    (integer23 + nodeEntity.x).toInt(),
-                    (integer24 + nodeEntity.y).toInt(),
-                    (integer25 + nodeEntity.z).toInt()
+                result = Position(
+                    (x + nodeEntity.x).toInt(),
+                    (y + nodeEntity.y).toInt(),
+                    (z + nodeEntity.z).toInt()
                 )
-                if (fx26.y >= 0 && (!boolean16 || nodeEntity.isWithinRestriction(
-                        fx26
-                    )) && (!boolean12 || (navigation.getTypeAsWalkable(nodeEntity.location.world ,fx26)) == PathType.WALKABLE)
+                if (result.y >= 0 && (!hasRestriction || nodeEntity.isWithinRestriction(
+                        result
+                    )) && (!boolean12 || (navigation.getTypeAsWalkable(
+                        nodeEntity.location.world,
+                        result
+                    )) == PathType.WALKABLE)
                 ) {
                     if (boolean9) {
-                        fx26 = moveUpToAboveSolid(
-                            fx26, random15.nextInt(integer10 + 1) + integer11, 256
-                        ) { fx ->
-                            nodeEntity.location.world.getBlockAt(fx.toLocation(nodeEntity.location.world)).type.isSolid
+                        result = moveUpToAboveSolid(
+                            result, random.nextInt(integer10 + 1) + integer11, 256
+                        ) { pos ->
+                            nodeEntity.location.world.getBlockAt(pos.toLocation(nodeEntity.location.world)).type.isSolid
                         }
                     }
-                    if (boolean6 || nodeEntity.location.world.getBlockAt(fx26.toLocation(nodeEntity.location.world)).type != Material.WATER) {
-                        val cww27: PathType = PathTypeFactory.getBlockPathTypeStatic(nodeEntity.location.world, fx26)
-                        if (nodeEntity.getPathfindingMalus(cww27) == 0.0f) {
-                            val double28 = toDoubleFunction.applyAsDouble(fx26)
+                    if (boolean6 || nodeEntity.location.world.getBlockAt(result.toLocation(nodeEntity.location.world)).type != Material.WATER) {
+                        val type = PathTypeFactory.getBlockPathTypeStatic(nodeEntity.location.world, result)
+                        if (nodeEntity.getPathfindingMalus(type) == 0.0f) {
+                            val double28 = toDoubleFunction.applyAsDouble(result)
                             if (double28 > double18) {
                                 double18 = double28
-                                fx20 = fx26
-                                boolean17 = true
+                                position = result
+                                bool = true
                             }
                         }
                     }
                 }
             }
         }
-        return if (boolean17) {
-            fx20.bottomCenter()
+        return if (bool) {
+            position.bottomCenter()
         } else {
             null
         }
     }
 
-    private fun getRandomDelta(random: Random, integer2: Int, integer3: Int, integer4: Int, dck: Vector?, double6: Double): Position? {
-        return if (dck != null && double6 < Math.PI) {
-            val double8: Double = atan2(dck.z, dck.x) - 1.5707963705062866
+    private fun getRandomDelta(
+        random: Random,
+        integer2: Int,
+        integer3: Int,
+        integer4: Int,
+        vector: Vector?,
+        double6: Double
+    ): Position? {
+        return if (vector != null && double6 < Math.PI) {
+            val double8: Double = atan2(vector.z, vector.x) - 1.5707963705062866
             val double10 = double8 + (2.0f * random.nextFloat() - 1.0f).toDouble() * double6
             val double12 = sqrt(random.nextDouble()) * SQRT_OF_TWO * integer2.toDouble()
             val double14 = -double12 * sin(double10)
@@ -322,35 +339,35 @@ object RandomPos {
                 null
             }
         } else {
-            val integer8 = random.nextInt(2 * integer2 + 1) - integer2
-            val integer9 = random.nextInt(2 * integer3 + 1) - integer3 + integer4
-            val integer10 = random.nextInt(2 * integer2 + 1) - integer2
-            Position(integer8, integer9, integer10)
+            val x = random.nextInt(2 * integer2 + 1) - integer2
+            val y = random.nextInt(2 * integer3 + 1) - integer3 + integer4
+            val z = random.nextInt(2 * integer2 + 1) - integer2
+            Position(x, y, z)
         }
     }
 
-    fun moveUpToAboveSolid(fx: Position, integer2: Int, integer3: Int, predicate: Predicate<Position>): Position {
+    fun moveUpToAboveSolid(position: Position, integer2: Int, integer3: Int, predicate: Predicate<Position>): Position {
         return if (integer2 < 0) {
             throw IllegalArgumentException("aboveSolidAmount was $integer2, expected >= 0")
-        } else if (!predicate.test(fx)) {
-            fx
+        } else if (!predicate.test(position)) {
+            position
         } else {
-            var fx5: Position
-            fx5 = fx.up()
-            while (fx5.y < integer3 && predicate.test(fx5)) {
-                fx5 = fx5.up()
+            var pos2: Position
+            pos2 = position.up()
+            while (pos2.y < integer3 && predicate.test(pos2)) {
+                pos2 = pos2.up()
             }
-            var fx6: Position
-            var fx7: Position
-            fx6 = fx5
-            while (fx6.y < integer3 && fx6.y - fx5.y < integer2) {
-                fx7 = fx6.up()
-                if (predicate.test(fx7)) {
+            var result: Position
+            var temp: Position
+            result = pos2
+            while (result.y < integer3 && result.y - pos2.y < integer2) {
+                temp = result.up()
+                if (predicate.test(temp)) {
                     break
                 }
-                fx6 = fx7
+                result = temp
             }
-            fx6
+            result
         }
     }
 
